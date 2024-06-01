f3d8d543efeb495a2cdcc3f2de6f40a99e507d24
kienpro-dev <tienkiennropro@gmail.com>
2024-06-01 15:57:14 +0700

    fix persistence jakarta: Contest

--- src/main/java/com/example/projectbase/domain/entity/Contest.java
+++ src/main/java/com/example/projectbase/domain/entity/Contest.java
@@ -4,16 +4,16 @@ import com.example.projectbase.domain.entity.common.DateAuditing;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
 @Setter
-@Builder
 @Entity
 @Table(name = "contests")
 public class Contest extends DateAuditing {
@@ -28,24 +28,24 @@ public class Contest extends DateAuditing {
     private LocalDateTime expiredDate;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "FK_CONTEST_USER"))
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
     @JsonIgnore
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "contests_users",
-            joinColumns = @JoinColumn(name = "contest_id", foreignKey = @ForeignKey(name = "FK_CONTEST_USER1")),
-            inverseJoinColumns = @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "FK_CONTEST_USER2")))
+            joinColumns = @JoinColumn(name = "contest_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> contests_users = new ArrayList<>();
 
     @JsonIgnore
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "contests_exercises",
-            joinColumns = @JoinColumn(name = "contest_id", foreignKey = @ForeignKey(name = "FK_CONTEST_EXERCISE1")),
-            inverseJoinColumns = @JoinColumn(name = "exercise_id", foreignKey = @ForeignKey(name = "FK_CONTEST_EXERCISE2")))
+            joinColumns = @JoinColumn(name = "contest_id"),
+            inverseJoinColumns = @JoinColumn(name = "exercise_id"))
     private List<Exercise> contests_exercises = new ArrayList<>();
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "contest")
     @JsonIgnore
     private List<Submission> submissions;
 }
