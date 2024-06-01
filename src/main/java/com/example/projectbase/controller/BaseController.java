bd9de8fdd737bef67c7e895c98ff9929abc4ed78
kienpro-dev <tienkiennropro@gmail.com>
2024-06-01 15:51:37 +0700

    add base controller

---
+++ src/main/java/com/example/projectbase/controller/BaseController.java
@@ -0,0 +1,19 @@
+package com.example.projectbase.controller;
+
+import com.example.projectbase.domain.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public abstract class BaseController {
+    @ModelAttribute("currentUser")
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof User) {
+            return (User) authentication.getPrincipal();
+        }
+        return null;
+    }
+}
