0cc5e173f8ab7ad070832a1f4a943a9c4ffbd48a
kienpro-dev <tienkiennropro@gmail.com>
2024-06-06 00:21:38 +0700

    fix bean date converter

--- src/main/java/com/example/projectbase/converter/DateTimeConverter.java
+++ src/main/java/com/example/projectbase/converter/DateTimeConverter.java
@@ -1,19 +1,28 @@
 package com.example.projectbase.converter;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DateTimeConverter implements Converter<Date, String> {
-    private final SimpleDateFormat formatter;
-
-    public DateTimeConverter(String pattern) {
-        this.formatter = new SimpleDateFormat(pattern);
-    }
+@Component
+public class DateTimeConverter implements Converter<String, Date> {
+    private static final String DATE_FORMAT = "HH:mm:ss, dd/MM/yyyy";
 
     @Override
-    public String convert(Date date) {
-        return formatter.format(date);
+    public Date convert(String source) {
+        if ( source.isEmpty()) {
+            return null;
+        }
+
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+            return dateFormat.parse(source);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
