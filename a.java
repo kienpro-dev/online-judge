1c68081b96802cab86c998c7ab24e807c2b0a9cd
kienpro-dev <tienkiennropro@gmail.com>
2024-06-07 01:56:16 +0700

    fix html ui: navigate

---
+++ a.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        double x = a * 1.0/b;
+        System.out.println(x);
+
+    }
+
+}
