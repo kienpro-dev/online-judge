e6e36a66c6d842fd138645f507a08c8d1dd69baf
kienpro-dev <tienkiennropro@gmail.com>
2024-06-11 17:16:04 +0700

    add controller edit profile

--- src/main/java/com/example/projectbase/controller/UserController.java
+++ src/main/java/com/example/projectbase/controller/UserController.java
@@ -2,17 +2,20 @@ package com.example.projectbase.controller;
 
 import com.example.projectbase.base.UiV1;
 import com.example.projectbase.constant.UrlConstant;
+import com.example.projectbase.domain.dto.request.UserUpdateDto;
 import com.example.projectbase.domain.entity.User;
 import com.example.projectbase.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @UiV1
 @AllArgsConstructor
-public class UserController extends BaseController{
+public class UserController extends BaseController {
     @Autowired
     private UserService userService;
 
@@ -21,5 +24,23 @@ public class UserController extends BaseController{
         User user = userService.findUserById(id);
         model.addAttribute("user", user);
         return "perinformation";
+    }
+
+    @GetMapping(UrlConstant.User.EDIT_USER_PARAM)
+    public String getPageEdit(Model model, @RequestParam(name = "id") Long id) {
+        User user = userService.findUserById(id);
+        model.addAttribute("user", user);
+        return "edit_profile";
+    }
+
+    @PostMapping(UrlConstant.User.EDIT_USER_PARAM)
+    public String updateUser(Model model, @RequestParam(name = "id") Long id, @ModelAttribute UserUpdateDto userUpdateDto) {
+        User user = userService.updateUser(id, userUpdateDto);
+        if (user != null) {
+            model.addAttribute("success", "Update user successfully!");
+        } else {
+            model.addAttribute("failed", "Update user failed!");
+        }
+        return "redirect:/user/edit(id=id)";
     }
 }
